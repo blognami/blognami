d37dfa793befc0752547704c2c18b48b6343b95a
Jody Salt <jody@jodysalt.com>
2021-11-28 15:08:41 +0000

    feat: after adding post redirect to page

--- packages/blognami/lib/views/admin/add_post.js
+++ packages/blognami/lib/views/admin/add_post.js
@@ -1,4 +1,9 @@
 
-export default ({ renderForm, posts }) => renderForm(posts, {
-    fields: ['userId', 'title']
+export default ({ renderForm, posts, renderHtml }) => renderForm(posts, {
+    fields: ['userId', 'title'],
+    success({ slug }){
+        return renderHtml`
+            <span data-action="load" data-url="/${slug}" data-target="_top"></span>
+        `;
+    }
 });
