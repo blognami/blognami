c747eaae673c82f8106f6038fe0d5d29abaaa8d4
Jody Salt <jody@jodysalt.com>
2023-01-21 10:54:16 +0000

    fix: external links

--- packages/pinstripe/lib/components/pinstripe_anchor.js
+++ packages/pinstripe/lib/components/pinstripe_anchor.js
@@ -8,9 +8,10 @@ export default {
         const { ignoreEventsFromChildren = false } = this.data;
         this.on('click', (event) => {
             if(ignoreEventsFromChildren && event.target != this) return;
+            const { action = 'load', confirm, target = '_self', method = 'GET', href } = { ...this.attributes, ...this.data };
+            if(new URL(href, window.location.href).host != window.location.host) return;
             event.preventDefault();
             event.stopPropagation();
-            const { action = 'load', confirm, target = '_self', method = 'GET', href } = { ...this.attributes, ...this.data };
             if(action == 'load') loadFrame.call(this, confirm, target, method, href);
             if(action == 'remove') removeFrame.call(this, confirm, target);
         });
