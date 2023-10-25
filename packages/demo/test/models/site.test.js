b9f054f694093bd64f45999d13809b7a38d5d195
Jody Salt <jody@jodysalt.com>
2023-10-25 23:03:57 +0100

    fix: database singletons only get created if tenant exists (#36)

--- packages/demo/test/models/site.test.js
+++ packages/demo/test/models/site.test.js
@@ -13,3 +13,32 @@ test(`site`, () => Workspace.run(async _ => {
 
     expect(await sites.count()).toBe(1);
 }));
+
+if(process.env.TENANCY == 'multi'){
+    test(`site with multi-tenancy and tenant exists`, () => Workspace.run(async _ => {
+        const { tenant, site, sites } = _.database;
+
+        expect(typeof await tenant).toBe('object');
+
+        expect(typeof await site).toBe('object');
+
+        expect(await sites.count()).toBe(1);
+
+        expect(await _.database.run(`select * from sites`).length).toBe(1);
+    }));
+    
+
+    test(`site with multi-tenancy and tenant does not exist`, () => Workspace.run(async _ => {
+        _.initialParams._headers.host = 'example.com';
+        
+        const { tenant, site, sites } = _.database;
+
+        expect(typeof await tenant).toBe('undefined');
+
+        expect(typeof await site).toBe('undefined');
+
+        expect(await sites.count()).toBe(0);
+
+        expect(await _.database.run(`select * from sites`).length).toBe(0);
+    }));
+}
