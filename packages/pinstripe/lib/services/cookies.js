45e3cea23a8b31bfeb66071d5b7b4db263c5a60d
Jody Salt <jody@jodysalt.com>
2023-11-22 07:43:13 +0000

    fix: cookies

--- packages/pinstripe/lib/services/cookies.js
+++ packages/pinstripe/lib/services/cookies.js
@@ -3,7 +3,7 @@ export default {
     create(){
         if(!this.context.root.hasOwnProperty('cookies')){
             const cookies = {};
-            const cookieHeader = this.params._headers?.cookie;
+            const cookieHeader = this.initialParams._headers?.cookie;
             if(cookieHeader){
                 cookieHeader.split(/;/).forEach(cookie => {
                     const matches = cookie.trim().match(/^([^=]+)=(.*)$/);
