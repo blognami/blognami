f1bbd2c644be5297ec085ee3469f2acb0578cb21
Jody Salt <jody@jodysalt.com>
2024-03-09 12:07:13 +0000

    wip

--- packages/blognami/lib/views/main/_actions/admin/edit_site_description.js
+++ packages/blognami/lib/views/main/_actions/admin/edit_site_description.js
@@ -2,7 +2,10 @@
 export default {
     async render(){
         return this.renderForm(this.database.site, {
-            fields: [{ name: 'description', type: '_markdown_editor' }]
+            fields: [
+                { name: 'revisionUserId', type: 'forced', value: this.session.user.id },
+                { name: 'description', type: '_markdown_editor' }
+            ]
         });
     }
 }
