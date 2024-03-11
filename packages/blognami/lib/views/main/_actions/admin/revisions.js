f156dd57f690b94f90f8a5a2431411a3579117c9
Jody Salt <jody@jodysalt.com>
2024-03-11 21:50:35 +0000

    wip

---
+++ packages/blognami/lib/views/main/_actions/admin/revisions.js
@@ -0,0 +1,10 @@
+
+export default {
+    async render(){
+        return this.renderHtml`
+            <pinstripe-modal>
+                <p>Coming soon!</p>
+            </pinstripe-modal>
+        `;
+    }
+};
