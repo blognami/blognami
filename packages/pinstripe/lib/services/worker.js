a815902fefc9c433181d639a91bb9d2498de81df
Jody Salt <jody@jodysalt.com>
2024-08-04 23:04:05 +0100

    wip

--- packages/pinstripe/lib/services/worker.js
+++ packages/pinstripe/lib/services/worker.js
@@ -31,40 +31,47 @@ export const client = {
     },
 
     async extractParams(request){
-        const { method, url, headers } = request;
-        const _url = new URL(url, 'http://127.0.0.1');
-
-        const urlParams = {};
-        _url.searchParams.forEach((value, key) => {
-            urlParams[key] = value;
-        });
-
-        const body = method.match(/^POST|PUT|PATCH$/) ? await this.parseBody(request) : {};
-        
         return {
-            ...urlParams,
-            ...body,
-            _method: method,
-            _url,
-            _headers: this.normalizeHeaders(headers)
+            ...(await this.extractUrlParams(request)),
+            ...(await this.extractBodyParams(request)),
+            _method: request.method,
+            _url: this.extractUrl(request),
+            _headers: this.extractHeaders(request)
         };
     },
 
-    normalizeHeaders(headers){
+    extractUrl(request){
+        return new URL(request.url, 'http://127.0.0.1');
+    },
+
+    extractUrlParams(request){
         const out = {};
-        for(let [key, value] of headers){
+        this.extractUrl(request).searchParams.forEach((value, key) => {
+            out[key] = value;
+        });
+        return out;
+    },
+
+    extractHeaders(request){
+        const out = {};
+        for(let [key, value] of request.headers){
             out[key] = value;
         }
         return out;
     },
 
-    async parseBody(request){
-        const body = await request.text();
-        const contentType = request.headers.get('content-type');
-        if(contentType == 'application/json'){
-            return JSON.parse(body);
+    async extractBodyParams(request){
+        if(!request.method.match(/^POST|PUT|PATCH$/)) return {};
+        const contentType = request.headers.get('content-type') ?? '';
+        if(contentType == 'application/json') return request.json();
+        const out = {};
+        if(contentType.match(/^multipart\/form-data/)){
+            const formData = await request.formData();
+            for(let [key, value] of formData){
+                out[key] = value;
+            }
         }
-        return {};
+        return out;
     }
 };
 
