968d39972c78f57bb8020c9888220a6d80579890
Jody Salt <jody@jodysalt.com>
2024-08-04 14:56:20 +0100

    wip

---
+++ packages/pinstripe/lib/service_file_importers/js.js
@@ -0,0 +1,39 @@
+
+import { ServiceFactory } from '../service_factory.js';
+import { Bundle } from '../bundle.js'; // pinstripe-if-client: const Bundle = undefined;
+import { fileURLToPath } from 'url'; // pinstripe-if-client: const fileURLToPath = undefined;
+import { inflector } from '../inflector.js';
+
+ServiceFactory.FileImporter.register('js', {
+    async importFile({ filePath, relativeFilePathWithoutExtension }){
+        if(relativeFilePathWithoutExtension == '_file_importer') return;
+
+        const { default: _default, client } = (await import(filePath));
+
+        if(_default || client) ServiceFactory.register(relativeFilePathWithoutExtension, {
+            meta(){
+                this.filePaths.push(filePath);
+                if(_default) this.include(_default);
+            }
+        });
+
+        if(client) Bundle.addModule('worker', `
+            import { ServiceFactory } from ${JSON.stringify(fileURLToPath(`${import.meta.url}/../../index.js`))};
+            import { client } from ${JSON.stringify(filePath)};
+            ServiceFactory.register(${JSON.stringify(relativeFilePathWithoutExtension)}, {
+                meta(){
+                    this.filePaths.push(${JSON.stringify(filePath)});
+                    this.include(client);
+                }
+            });
+        `);
+    }
+});
+
+export function createDecoratorsInclude(hash, decorators){
+    const out = {};
+    Object.keys(decorators).forEach(key => {
+        out[`.view-${hash}-${inflector.dasherize(key)}`] = decorators[key];
+    });
+    return out;
+}
