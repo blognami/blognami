b034a7840aede5a1aed6e2ff56badd39ceef1423
Jody Salt <jody@jodysalt.com>
2024-09-05 22:21:46 +0100

    wip

---
+++ packages/blognami/lib/migrations/1725555607_create_membership_tiers.js
@@ -0,0 +1,12 @@
+
+export default {
+    async migrate(){
+        await this.database.table('membershipTiers', async membershipTiers => {
+            await membershipTiers.addColumn('enableMonthly', 'boolean');
+            await membershipTiers.addColumn('monthlyPrice', 'decimal');
+            await membershipTiers.addColumn('enableYearly', 'boolean');
+            await membershipTiers.addColumn('yearlyPrice', 'decimal');
+            await membershipTiers.addColumn('enableFree', 'boolean');
+        });
+    }
+};
