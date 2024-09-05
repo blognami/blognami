217b365ecb7604844b2c3695cce80cc506e573f0
Jody Salt <jody@jodysalt.com>
2024-09-05 22:26:47 +0100

    wip

--- packages/blognami/lib/migrations/1644164186_create_site.js
+++ packages/blognami/lib/migrations/1644164186_create_site.js
@@ -4,11 +4,6 @@ export default {
         await this.database.table('sites', async sites => {
             await sites.addColumn('title', 'string');
             await sites.addColumn('description', 'text');
-            await sites.addColumn('enableMonthlyPaidSubscriptions', 'boolean');
-            await sites.addColumn('monthlyPaidSubscriptionPrice', 'decimal');    
-            await sites.addColumn('enableYearlyPaidSubscriptions', 'boolean');   
-            await sites.addColumn('yearlyPaidSubscriptionPrice', 'decimal');
-            await sites.addColumn('enableFreeSubscriptions', 'boolean');
             await sites.addColumn('salt', 'string');
         });
     }
