e735f512afae9d8a6da26919dd493b76a8660f05
Jody Salt <jody@jodysalt.com>
2024-09-15 21:57:16 +0100

    wip

---
+++ packages/blognami/lib/views/main/_subscription_cta.js
@@ -0,0 +1,24 @@
+
+export default {
+    render(){
+        const { access } = this.params;
+        
+        return this.renderHtml`
+            ${() => {
+                if (access == 'free') return this.renderHtml`
+                    <p>This content is for subscribers only.</p>
+                `;
+                return this.renderHtml`
+                    <p>This content is for paying subscribers only.</p>
+                `;
+            }}
+            ${this.renderView(`_button`, {
+                tagName: 'a',
+                href: '/_actions/subscribe',
+                target: '_overlay',
+                isPrimary: true,
+                body: 'Subscribe now',
+            })}
+        `;        
+    }
+};
