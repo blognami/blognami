90ba00e82b068229b2366cff441ce5a2cbe83627
Jody Salt <jody@jodysalt.com>
2024-12-19 16:06:44 +0000

    refactor: removed themes

--- packages/blognami/lib/views/_services/theme.js
+++
@@ -1,6 +0,0 @@
-
-export default {
-    async render(){
-        return [200, { 'content-type': 'application/json' }, [ JSON.stringify( await this.theme ) ]];
-    }
-}
--- packages/pinstripe/lib/services/render_view.js
+++ packages/pinstripe/lib/services/render_view.js
@@ -6,10 +6,34 @@ export const client = true;
 export default {
     create(){
         return async (name, params = {}) => {
-            const viewMap = await this.viewMap;
-            const mappedName = viewMap[name];
+            const mappedName = this.viewMap[name];
             if(!mappedName) return;
             return View.render(this.context, mappedName, params);
         }
+    },
+
+    get viewMap(){
+        const cacheKey = 'viewMap';
+        if(!View.cache[cacheKey]){
+            View.cache[cacheKey] = this.createViewMap();
+        }
+        return View.cache[cacheKey];
+    },
+
+    createViewMap(){
+        const out = {}
+
+        for(let viewName of View.names){
+            out[viewName] = viewName;
+        }
+
+        for(let viewName of Object.keys(out)){
+            if(viewName.endsWith('/index')){
+                const overrideName = viewName.replace(/\/index$/, '');
+                out[overrideName] ??= viewName;
+            }
+        }
+        
+        return out;
     }
 };
