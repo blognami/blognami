693e7015b939010598029bd3012501e9ca5241d3
Jody Salt <jody@jodysalt.com>
2025-03-06 22:01:29 +0000

    wip

--- packages/@blognami/main/lib/views/main/_actions/user/your_account/_subscription.js
+++ packages/@blognami/main/lib/views/main/_actions/user/your_account/_subscription.js
@@ -11,7 +11,7 @@ export default {
 
         return this.renderHtml`
             ${() => {
-                if(user.membershipTier != 'none' || true) return this.renderHtml`
+                if(user.membershipTier != 'none') return this.renderHtml`
                     <a href="/_actions/user/unsubscribe" target="_overlay" data-test-id="unsubscribe" data-confirm="${user.membershipTier == 'paid' ? (
                         `Are you sure you want to unsubscribe? You will lose access to members only content, and any remaining subscription time will be lost.`
                     ):(
