cc214f855a55d68a9761851db558c3e1434e45cc
Jody Salt <jody@jodysalt.com>
2025-03-06 21:30:52 +0000

    wip

---
+++ packages/@blognami/main/lib/views/main/_actions/admin/edit_settings/_site_meta.js
@@ -0,0 +1,8 @@
+
+export default {
+    render(){
+        return this.renderHtml`
+            <a href="/_actions/admin/add_page" target="_overlay" data-test-id="add-page">Page</a>
+        `;
+    }
+};
