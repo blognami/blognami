278b8513542cc72da0d05b624796ee24bcffa497
Jody Salt <jody@jodysalt.com>
2025-03-29 14:45:35 +0000

    wip

--- packages/blognami/lib/text.js
+++ packages/blognami/lib/text.js
@@ -14,47 +14,11 @@ export const Text = Class.extend().include({
         this.outputBuffer = [];
     },
 
-    echo(stringable){
-        this.outputBuffer.push((async () => `${await stringable}`)());
-    },
-    
-    line(stringable = ''){
-        if(this.outputBuffer.length){
-            this.echo((async () => `\n${await stringable}`)());
-        } else {
-            this.echo(stringable);
-        }
-    },
-
-    indent(fn){
-        this.outputBuffer.push((async () => {
-            const lines = await this.grab(fn);
-
-            if(!lines.length){
-                return;
-            }
-
-            let out = [];
-
-            lines.join('').split(/\n/).forEach((string) => {
-                out.push(`    ${string}`);
-            });
-
-            out = out.join('\n');
-
-            return this.outputBuffer.length - 1 > 0 ? `\n${out}` : out;
-        })());
-    },
-
-    async grab(fn){
-        return (await this.constructor.render(fn)).outputBuffer;
-    },
-
     async render(fn){
         await fn({
-            echo: this.echo.bind(this),
-            line: this.line.bind(this),
-            indent: this.indent.bind(this)
+            echo: echo.bind(this),
+            line: line.bind(this),
+            indent: indent.bind(this)
         });
 
         this.outputBuffer = await Promise.all(this.outputBuffer);
@@ -70,3 +34,36 @@ export const Text = Class.extend().include({
         return [status, {'content-type': 'text/plain', ...headers}, [this.value.trim()]];
     }
 });
+
+
+function echo(stringable){
+    this.outputBuffer.push((async () => `${await stringable}`)());
+}
+
+function line(stringable = ''){
+    if(this.outputBuffer.length){
+        echo.call(this, (async () => `\n${await stringable}`)());
+    } else {
+        echo.call(this, stringable);
+    }
+}
+
+function indent(fn){
+    this.outputBuffer.push((async () => {
+        const lines = (await this.constructor.render(fn)).outputBuffer;
+
+        if(!lines.length){
+            return;
+        }
+
+        let out = [];
+
+        lines.join('').split(/\n/).forEach((string) => {
+            out.push(`    ${string}`);
+        });
+
+        out = out.join('\n');
+
+        return this.outputBuffer.length - 1 > 0 ? `\n${out}` : out;
+    })());
+}
