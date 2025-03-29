1d3589ac287166c9099710e8a607492f8be1ada3
Jody Salt <jody@jodysalt.com>
2025-03-29 14:59:33 +0000

    wip

--- packages/blognami/lib/services/fs_builder.js
+++ packages/blognami/lib/services/fs_builder.js
@@ -128,5 +128,82 @@ export default {
             generateFile,
             inProjectRootDir
         };
+    },
+
+    async confirm(question){
+        while(true){
+            const answer = (await new Promise((resolve) => {
+                const rl = readline.createInterface({
+                    input: process.stdin,
+                    output: process.stdout
+                });
+                rl.question(`${question.replace(/\?+$/, '')}? Y/n/a: `, answer => {
+                    rl.close();
+                    resolve(answer);
+                });
+            })).toLowerCase();
+            
+            if(answer == '' || answer == 'y'){
+                return true;
+            } else if(answer == 'n'){
+                return false;
+            } else if(answer == 'a'){
+                process.exit();
+            }
+        }
+    },
+
+    async generateDir(dirPath, fn = () => {}){
+        if(!path.isAbsolute(dirPath)){
+            dirPath = path.normalize(`${process.cwd()}/${dirPath}`);
+        }
+        if(!fs.existsSync(dirPath)){
+            console.log(chalk.green(`Creating dir "${dirPath}"`));
+            const dirSegments = dirPath.replace(/^\//, '').split(/\//);
+            const currentDirSegments = [];
+            while(dirSegments.length){
+                currentDirSegments.push(dirSegments.shift());
+                const currentPath = `/${currentDirSegments.join('/')}`;
+                if(!fs.existsSync(currentPath)){
+                    await promisify(fs.mkdir)(currentPath);
+                }
+            }
+        }
+
+        const previousDirPath = process.cwd();
+        process.chdir(dirPath);
+        await fn();
+        process.chdir(previousDirPath);
+    },
+
+    async generateFile(name, ...args){
+        const options = typeof args[0] == 'object' ? args.shift() : {};
+        const fn = typeof args[0] == 'function' ? args.shift() : () => {};
+        const matches = name.match(/^(.*)\/([^\/]*)$/);
+        if(matches){
+            await generateDir(matches[1], () => generateFile(matches[2], options, fn));
+        } else {
+            const data = (grab(fn)).join('');
+            const filePath = `${process.cwd()}/${name}`;
+
+            if(!fs.existsSync(filePath)){
+                console.log(chalk.green(`Creating file "${filePath}"`));
+                await promisify(fs.writeFile)(name, data);
+            } else if(await promisify(fs.readFile)(name) == data){
+                // no change (so ignore)
+            } else if(options.skipIfExists){
+                console.log(chalk.blue(`Skipping file "${filePath}" (as it already exists)`));
+            } else if(options.force || await confirm(`Are you sure you want to update ${filePath}?`)){
+                console.log(chalk.red(`Updating file "${filePath}"`));
+                await promisify(fs.writeFile)(name, data);
+            }
+        }
+    },
+
+    async inProjectRootDir(fn){
+        const previousDirPath = process.cwd();
+        process.chdir(await this.project.rootPath);
+        await fn();
+        process.chdir(previousDirPath);
     }
 };
