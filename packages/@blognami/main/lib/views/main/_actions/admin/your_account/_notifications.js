307222210af40b0f832c01fb704e26909a44ad32
Jody Salt <jody@jodysalt.com>
2025-05-04 16:04:21 +0100

    wip

--- packages/@blognami/main/lib/views/main/_actions/admin/your_account/_notifications.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-    async render(){
-        return this.renderHtml`
-            <a href="/_actions/user/notifications" target="_overlay" data-test-id="notifications">Notifications</a>
-        `;
-    }
-};
