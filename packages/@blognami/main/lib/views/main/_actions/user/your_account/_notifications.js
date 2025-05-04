c04bfe6a36559d0e9e169a34a1a99d201d68fde1
Jody Salt <jody@jodysalt.com>
2025-05-04 15:52:00 +0100

    wip

--- packages/@blognami/main/lib/views/main/_actions/user/your_account/_notifications.js
+++ packages/@blognami/main/lib/views/main/_actions/user/your_account/_notifications.js
@@ -1,10 +1,8 @@
 
 export default {
     async render(){
-        const user = await this.session.user;
-        
         return this.renderHtml`
-            <a href="/_actions/user/notifications" target="_top" data-test-id="notifications">Notifications</a>
+            <a href="/_actions/user/notifications" target="_overlay" data-test-id="notifications">Notifications</a>
         `;
     }
 };
