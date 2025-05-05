2266eeaf8eae5f509e8410291d226cef9aaad51b
Jody Salt <jody@jodysalt.com>
2025-05-05 10:54:06 +0100

    wip

--- packages/@blognami/main/lib/views/main/_actions/admin/your_account/_notification_preferences.js
+++ packages/@blognami/main/lib/views/main/_actions/admin/your_account/_notification_preferences.js
@@ -2,7 +2,7 @@
 export default {
     async render(){
         return this.renderHtml`
-            <a href="/_actions/user/edit_notification_preferences" target="_overlay" data-test-id="edit-notification-preferences">Notification preferences</a>
+            <a href="/_actions/user/edit_user_notification_preferences" target="_overlay" data-test-id="edit-user-notification-preferences">Notification preferences</a>
         `;
     }
 };
