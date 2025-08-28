b68858fe140fd4772b9b785a23c1c817f2f36ac8
Jody Salt <jody@jodysalt.com>
2025-08-28 22:31:06 +0100

    wip

--- packages/pinstripe/lib/style_rule.js
+++ packages/pinstripe/lib/style_rule.js
@@ -82,7 +82,7 @@ export const StyleRule = Class.extend().include({
     },
 
     apply(){
-
+        this.styles[this.constructor.name] = this.value;
     }
 });
 
--- packages/pinstripe/lib/style_rule.test.js
+++ packages/pinstripe/lib/style_rule.test.js
@@ -37,6 +37,34 @@ test(`StyleRule.normalizeRules`, () => {
     assert.deepEqual(StyleRule.normalizeRules(['color: blue']), [{name: 'color', value: 'blue'}]);
 });
 
+test(`StyleRule.compileRules`, () => {
+    const createStub = () => ({
+        calls: [],
+        styles: {},
+        create(...value){
+            this.calls.push(['create', ...value]);
+            return this;
+        },
+        apply(){
+            this.calls.push(['apply']);
+        }
+    });
+
+    let stub = createStub();
+    StyleRule.compileRules('background: yellow').call(stub, 'STUB_STYLES');
+    assert.deepEqual(stub.calls, [
+        ['create', 'background', 'STUB_STYLES', 'yellow'],
+        ['apply']
+    ]);
+    
+    stub = createStub();
+    StyleRule.compileRules('background: rgb(123, 456, 789); P; color: blue').call(stub, 'STUB_STYLES');
+    assert.deepEqual(stub.calls, [
+        ['create', 'background', 'STUB_STYLES', 'rgb(123, 456, 789)'], ['apply'],
+        ['create', 'P', 'STUB_STYLES', ''], ['apply'],
+        ['create', 'color', 'STUB_STYLES', 'blue'], ['apply']
+    ]);
+});
 
 // test(`StyleRule.parseRules`, () => {
 //     assert.deepEqual(StyleRule.parseRules(''), []);
