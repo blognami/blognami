01fa38bc2871048c26b063ee0ba590dc0013d2b6
Jody Salt <jody@jodysalt.com>
2025-09-04 23:13:47 +0100

    wip

--- packages/sintra/lib/theme.js
+++ packages/sintra/lib/theme.js
@@ -11,22 +11,26 @@ export const Theme = Class.extend().include({
     this.include(themeVariables);
   },
 
-  // colors: {
-  //   accent: 'oklch(59.2% 0.249 0.584)', // eventually we can use a @colors.pink.600 reference here
-  //   primaryText: '#000',
-  //   secondaryText: '#757575',
-  //   lighterGray: '#f6f6f6',
-  //   lightGray: '#e6e6e6',
-  //   midGray: '#ccc',
-  //   darkGray: '#444',
-  //   darkerGray: '#15171a'
-  // },
+  colors: {
+    accent: 'oklch(59.2% 0.249 0.584)', // eventually we can use a @colors.pink.600 reference here
+    primaryText: '#000',
+    secondaryText: '#757575',
+    lighterGray: '#f6f6f6',
+    lightGray: '#e6e6e6',
+    midGray: '#ccc',
+    darkGray: '#444',
+    darkerGray: '#15171a'
+  },
 
   // fonts: {
   //   sans: '-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif',
   //   serif: 'Times, serif',
   //   mono: 'Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace'
   // },
+
+  deepMerge(variables){
+    deepMerge(this, variables);
+  },
 
   resolveReferences(){
     // will resolves any references in the theme object e.g. @colors.pink.600
@@ -37,4 +41,16 @@ export const Theme = Class.extend().include({
 
 function remify(value) {
   return `${parseInt(value) / 16}rem`;
+}
+
+function deepMerge(target, source) {
+  for (const key of Object.keys(source)) {
+    if (source[key] instanceof Object && key in target) {
+      Object.assign(source[key], deepMerge(target[key], source[key]));
+    }
+  }
+
+  // Join `target` and modified `source`
+  Object.assign(target || {}, source);
+  return target;
 }
