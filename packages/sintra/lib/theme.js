8ea1e0f9a82ef0f6f6f9f9f8ab21064277b4b813
Jody Salt <jody@jodysalt.com>
2025-09-04 22:58:53 +0100

    wip

--- packages/sintra/lib/theme.js
+++ packages/sintra/lib/theme.js
@@ -8,26 +8,22 @@ export const Theme = Class.extend().include({
     this.include(Singleton);
   },
 
-  colors: {
-    accent: 'oklch(59.2% 0.249 0.584)', // eventually we can use a @colors.pink.600 reference here
-    primaryText: '#000',
-    secondaryText: '#757575',
-    lighterGray: '#f6f6f6',
-    lightGray: '#e6e6e6',
-    midGray: '#ccc',
-    darkGray: '#444',
-    darkerGray: '#15171a'
-  },
+  // colors: {
+  //   accent: 'oklch(59.2% 0.249 0.584)', // eventually we can use a @colors.pink.600 reference here
+  //   primaryText: '#000',
+  //   secondaryText: '#757575',
+  //   lighterGray: '#f6f6f6',
+  //   lightGray: '#e6e6e6',
+  //   midGray: '#ccc',
+  //   darkGray: '#444',
+  //   darkerGray: '#15171a'
+  // },
 
-  fonts: {
-    sans: '-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif',
-    serif: 'Times, serif',
-    mono: 'Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace'
-  },
-
-  spacing: {},
-
-  breakpoints: {},
+  // fonts: {
+  //   sans: '-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif',
+  //   serif: 'Times, serif',
+  //   mono: 'Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace'
+  // },
 
   resolveReferences(){
     // will resolves any references in the theme object e.g. @colors.pink.600
