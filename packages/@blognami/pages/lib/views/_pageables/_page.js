63dae16924cda5aed610415fddeebe142adc95bd
Jody Salt <jody@jodysalt.com>
2025-09-09 21:25:24 +0100

    wip

--- packages/@blognami/pages/lib/views/_pageables/_page.js
+++ packages/@blognami/pages/lib/views/_pageables/_page.js
@@ -54,7 +54,7 @@ export default {
         meta.push({ title: page.metaTitle || page.title });
         if(page.metaDescription) meta.push({ name: 'description', content: page.metaDescription });
 
-        const userHasAccess = post.access == 'public' || await user?.isSubscribedToNewsletter({ tier: page.access });
+        const userHasAccess = page.access == 'public' || await user?.isSubscribedToNewsletter({ tier: page.access });
     
         return this.renderView('_layout', {
             meta,
