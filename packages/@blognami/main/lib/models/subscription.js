9a04bb9b51d4349b429457beb706b1969d1160cf
Jody Salt <jody@jodysalt.com>
2025-09-10 14:53:22 +0100

    wip

--- packages/@blognami/main/lib/models/subscription.js
+++ packages/@blognami/main/lib/models/subscription.js
@@ -3,5 +3,8 @@ export default {
     meta(){
         this.belongsTo('subscribable');
         this.belongsTo('user');
+
+        this.mustNotBeBlank('tier');
+        this.mustMatchPattern('tier', /^(free|paid)$/);
     }
 }
