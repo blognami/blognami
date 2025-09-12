9ce13e95f12879d9e7790bb1690f169658442e29
Jody Salt <jody@jodysalt.com>
2025-09-12 23:36:48 +0100

    wip

---
+++ packages/demo/test/services/database.test.js
@@ -0,0 +1,19 @@
+
+import '../../lib/index.js';
+
+import test from 'node:test';
+import assert from 'node:assert';
+
+import { Workspace } from 'pinstripe';
+
+
+test(`database.lock`, () => Workspace.run(async function(){
+    await Promise.all([
+        this.database.lock(async () => {
+            await new Promise(resolve => setTimeout(resolve, 1));
+        }),
+        this.database.lock(async () => {
+            await new Promise(resolve => setTimeout(resolve, 1));
+        })
+    ])
+}));
