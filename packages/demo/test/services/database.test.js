25abac2fdf3171f322494e72cd5caa3a4c6a5d0c
Jody Salt <jody@jodysalt.com>
2025-09-12 23:37:19 +0100

    wip

--- packages/demo/test/services/database.test.js
+++ packages/demo/test/services/database.test.js
@@ -10,10 +10,10 @@ import { Workspace } from 'pinstripe';
 test(`database.lock`, () => Workspace.run(async function(){
     await Promise.all([
         this.database.lock(async () => {
-            await new Promise(resolve => setTimeout(resolve, 1));
+            await new Promise(resolve => setTimeout(resolve));
         }),
         this.database.lock(async () => {
-            await new Promise(resolve => setTimeout(resolve, 1));
+            await new Promise(resolve => setTimeout(resolve));
         })
     ])
 }));
