943955912cc2a59adfc321375d7af88a3cd0bf82
Jody Salt <jody@jodysalt.com>
2025-10-17 20:24:12 +0100

    wip

--- packages/@blognami/main/lib/views/index/index.js
+++ packages/@blognami/main/lib/views/index/index.js
@@ -1,5 +1,28 @@
 
 export default {
+    meta(){
+        this.on('renderBody', async function(){
+            let user;
+            if(await this.session){
+                user = await this.session.user;
+            }
+
+            const isAdmin = user?.role == 'admin';
+
+            const home = await this.database.home;
+        
+            if(isAdmin) return this.renderView('_editable_area', {
+                url: `/_actions/admin/edit_home_meta`,
+                body: this.renderHtml`
+                    <p><b>Meta title:</b> ${home.metaTitle}</p>
+                    <p><b>Meta description:</b> ${home.metaDescription}</p>
+                `,
+                linkTestId: "edit-home-meta",
+                bodyTestId: "home-meta"
+            }).assignProps({ displayOrder: 200 });
+        });
+    },
+
     async render(){
         const home = await this.database.home;
 
@@ -10,7 +33,7 @@ export default {
         return this.renderView('_layout', {
             meta,
             // body: this.renderViews('index/_*', this.params)
-            body: this.trigger('body')
+            body: this.trigger('renderBody')
         });
     }
 }
