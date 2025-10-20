1993282578a8a75d97bf1acf58d7b9a9de947be6
Jody Salt <jody@jodysalt.com>
2025-10-20 23:09:06 +0100

    wip

--- packages/@blognami/main/lib/views/_navbar/burger_menu/_about_section.js
+++ packages/@blognami/main/lib/views/_navbar/burger_menu/_about_section.js
@@ -39,13 +39,26 @@ export default {
     
         const site = await this.database.site;
 
+        let body = await this.renderMarkdown(await site.description);
+        
+        const parsedBody = this.parseHtml(body);
+
+        for(const [index, child] of Object.entries(parsedBody.children)) {
+            if(child.type != 'ul') continue;
+            const children = this.extractListItems(child);
+            const html = await this.renderView('_navbar/burger_menu/_link_group', { children });
+            parsedBody.children[index] = this.parseHtml(html);
+            parsedBody.children[index].parent = parsedBody;
+        }
+
+        body = await this.renderHtml(parsedBody.toString());
+
+        const wrappedContent = this.renderHtml`<div class="${this.cssClasses.aboutContent}">${body}</div>`;
+
         return this.renderView('_navbar/burger_menu/_section', {
             label: 'About',
             testId: 'about-section',
             body: async () => {
-                const content = this.renderMarkdown(await site.description);
-                const wrappedContent = this.renderHtml`<div class="${this.cssClasses.aboutContent}">${content}</div>`;
-                
                 if(isAdmin) return this.renderView('_editable_area', {
                     url: "/_actions/admin/edit_site_description",
                     body: wrappedContent
@@ -53,5 +66,36 @@ export default {
                 return wrappedContent;
             }
         });
+    },
+
+    extractListItems(ul) {
+        const out = [];
+        for(const li of ul.children) {
+            if(li.type !== 'li') continue;
+
+            const item = {};
+
+            const a = li.children.find(c => c.type === 'a');
+            if(a){
+                item.label = a.children.map(c => c.type === '#text' ? c.attributes.value : '').join('').trim();
+                item.url = a.attributes.href;
+                item.target = '_top';
+            } else {
+                item.label = li.children.map(c => c.type === '#text' ? c.attributes.value : '').join('').trim();
+            }
+
+            const children = [];
+
+            for(const child of li.children) {
+                if(child.type !== 'ul') continue;
+                children.push(...this.extractListItems(child));
+            }
+
+            if(children.length) item.children = children;
+
+            out.push(item);
+        }
+        return out;
     }
+
 };
