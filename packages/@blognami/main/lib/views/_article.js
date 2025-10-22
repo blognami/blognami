e9748fa230a370e8044d0c7786743bc48d43dd81
Jody Salt <jody@jodysalt.com>
2025-10-22 22:28:23 +0100

    wip

---
+++ packages/@blognami/main/lib/views/_article.js
@@ -0,0 +1,14 @@
+
+export const styles = ({ colors }) => `
+
+`;
+
+export default {
+    render(){
+        const { meta, title, body, footer } = this.params;
+
+        return this.renderHtml`
+            
+        `;
+    }
+}
