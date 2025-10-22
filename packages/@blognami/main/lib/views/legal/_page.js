f6b1874b0a1368243700e4f3df0202a48ee8c346
Jody Salt <jody@jodysalt.com>
2025-10-22 23:01:12 +0100

    wip

--- packages/@blognami/main/lib/views/legal/_page.js
+++ packages/@blognami/main/lib/views/legal/_page.js
@@ -1,20 +1,4 @@
 
-export const styles = `
-    .article {
-        max-width: 650px;
-    }
-    
-    .header {
-        margin-bottom: 4rem;
-    }
-
-    .title {
-        font-size: 7.4rem;
-        font-weight: 600;
-        line-height: 1;
-    }
-`;
-
 export default {
     async render(){
         const { title } = this.params;
@@ -33,25 +17,20 @@ export default {
             title,
             body: this.renderHtml`
                 <section>
-                    <article class="${this.cssClasses.article}">
-                        <header class="${this.cssClasses.header}">
-                            <h1>${title}</h1>
-                        </header>
-                        ${async () => {
-                            if(isAdmin) return this.renderView('_editable_area', {
-                                url: `/_actions/admin/edit_site_${this.inflector.snakeify(title)}`,
-                                body:  this.renderView('_content', {
-                                    body: this.renderMarkdown(await this.database.site[fieldName]),
-                                    testId
-                                }),
-                                linkTestId: `edit-${testId}`
-                            });
-                            return this.renderView('_content', {
+                    ${this.renderView('_article', {
+                        title: title,
+                        body: isAdmin ? this.renderView('_editable_area', {
+                            url: `/_actions/admin/edit_site_${this.inflector.snakeify(title)}`,
+                            body: this.renderView('_content', {
                                 body: this.renderMarkdown(await this.database.site[fieldName]),
                                 testId
-                            });
-                        }}
-                    </article>
+                            }),
+                            linkTestId: `edit-${testId}`
+                        }) : this.renderView('_content', {
+                            body: this.renderMarkdown(await this.database.site[fieldName]),
+                            testId
+                        })
+                    })}
                 </section>
             `
         });
