2268d5f2a8b17efa750ea303285fe61c752d8c34
Jody Salt <jody@jodysalt.com>
2025-10-24 22:18:41 +0100

    wip

---
+++ packages/@blognami/main/lib/views/_actions/user/delete_comment.js
@@ -0,0 +1,19 @@
+
+export default {
+    async render(){
+        const { id } = this.params;
+        const comment = await this.database.comments.where({ id }).first();
+        if(comment){
+            const user = await this.session.user;
+            await this.database.lock(async () => {
+                if(comment.userId != user.id && user.role != 'admin') return;
+                if(await comment.comments.count() > 0){
+                    await comment.update({ deleted: true });
+                } else {
+                    await comment.delete();
+                }
+            });
+        }
+        return this.renderRedirect({ target: '_top' });
+    }
+}
---
+++ packages/@blognami/main/lib/views/_actions/user/unsubscribe.js
@@ -0,0 +1,12 @@
+
+export default {
+    async render(){
+        const user = await this.session.user;
+        const subscribable = await this.database.subscribables.where({ id: this.params.subscribableId }).first();
+        if(!user || !subscribable) return;
+
+        await user.unsubscribeFrom(subscribable);
+
+        return this.renderRedirect({ target: '_top' });
+    }
+}
