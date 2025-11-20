0c9ca88e60dc34ace605d80f36146f805e7f44d2
Jody Salt <jody@jodysalt.com>
2025-11-20 22:48:47 +0000

    wip

--- packages/@pinstripe/markdown/lib/views/_markdown_editor/index.js
+++ packages/@pinstripe/markdown/lib/views/_markdown_editor/index.js
@@ -1,20 +1,18 @@
 
 export default {
     render(){
-        const { name, value, error, cssClasses, hasSaveChangesButton = false } = this.params;
+        const { name, value, error, cssClasses, saveChangesUrl } = this.params;
 
-        // convert this to renderTag?
-        return this.renderHtml`
-            <textarea
-                name="${name}"
-                class="${cssClasses.textarea} ${error ? cssClasses.isError : ''}"
-                data-component="pinstripe-anchor"
-                data-href="/_markdown_editor/modal"
-                data-target="_overlay"
-                data-preload
-                data-test-id="markdown-input"
-                ${hasSaveChangesButton ? 'data-has-save-changes-button="true"' : ''}
-            >${value}</textarea>
-        `;
+        return this.renderTag('textarea', {
+            name,
+            class: `${cssClasses.textarea} ${error ? cssClasses.isError : ''}`,
+            'data-component': 'pinstripe-anchor',
+            'data-href': '/_markdown_editor/modal',
+            'data-target': '_overlay',
+            'data-preload': true,
+            'data-test-id': 'markdown-input',
+            'data-save-changes-url': saveChangesUrl,
+            body: value
+        });
     }
 };
