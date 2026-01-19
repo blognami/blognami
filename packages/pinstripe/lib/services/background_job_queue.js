6cacdb6fb2e786c98e9be56d7bf9500d3b58a6d1
Jody Salt <jody@jodysalt.com>
2026-01-19 21:34:40 +0000

    refactor: use module-level queue in background job queue service
    Move queue from instance property to module constant for simpler state
    management.

--- packages/pinstripe/lib/services/background_job_queue.js
+++ packages/pinstripe/lib/services/background_job_queue.js
@@ -1,20 +1,20 @@
 
+const queue = [];
+
 export default {
     create(){
         return this.context.root.getOrCreate("backgroundJobQueue", () => this);
     },
 
-    _queue: [],
-
     async push(name, params = {}){
-        this._queue.push({ name, params });
+        queue.push({ name, params });
     },
 
     async shift(){
-        return this._queue.shift();
+        return queue.shift();
     },
 
     async length(){
-        return this._queue.length;
+        return queue.length;
     }
 };
